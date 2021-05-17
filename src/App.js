31263c3c0ad55c75ac6b20a55dd3d8d22446083b
raquelgraca <raquelngraca@gmail.com>
2021-05-16 23:07:04 -0300

    Deleted the entire div inside return and added Components created so far. Also added tasks array in App to make it global state - to use in order components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Create Status Report',
+      day: 'May 17th at 8:00am',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Doc Appoint',
+      day: 'May 17th at 12:00pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Finish Template',
+      day: 'May 17th at 02:00pm',
+      reminder: false,
+    }    
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
